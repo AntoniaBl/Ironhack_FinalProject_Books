96fc144109a7fce63acdcac5d95fe644af6b05e2
Antonia Bloch <antonia.bloch@q3k.de>
2021-11-22 00:25:11 +0100

    update

--- Functions_Books.py
+++ Functions_Books.py
@@ -74,13 +74,13 @@ def remove_punctuation(text):
 def combine_features(data):
     features=[]
     for i in range(0, data.shape[0]):
-        features.append( data['series_tokenized'][i] + ' ' + data['desccription_tokenized'][i] + ' ' + data['series_tokenized'][i])
+        features.append( data['title_tokenized'][i] + ' ' + data['desccription_tokenized'][i] + ' ' + data['series_tokenized'][i])
     return features
 
 #Model
 
-#UserInput Title recommendations
-def suggestions_user_input(title_user):
+#UserInput other title recommendations
+def suggestions_other_title(title_user):
     choices = df1['title'] 
     # Get a list of matches ordered by score, default limit to 5
     rec = process.extract(title_user, choices)
@@ -88,33 +88,58 @@ def suggestions_user_input(title_user):
     for i in rec:
         print(i[0])
 
+#Assume book title if fuzzy ratio is above 95
+def assume_book_title(title_user):
+    choices = df1['title'] 
+    # Get a list of matches ordered by score, default limit to 5
+    rec = process.extract(title_user, choices)
+    lst=[]
+    for i in rec:
+        lst.append(i[1])
+        #print(i[1])
+        if lst[0]>95:
+            title_user=i[0]#list.sort(key=lst, reverse=True)
+            print('I did not find an exact match in the database. I assume you mean the book: ' + title_user)
+            give_5bookrecommendationsCount(title_user)
+            break
+        else:
+            error
+    
+
 #Book Recommendations based on Wordcount
 
-def give_recommendationCount(title_user):
+def give_5bookrecommendationsCount(title_user):
+    #1.Step - convert the description from combined_features to a matrix of word counts
+    cm = CountVectorizer().fit_transform(df1['combined_features'])
+    #2.Get the cosine_simmilarity from the count matrix
+    cs = cosine_similarity(cm)
+    #Get index of title of user input
+    indices = df1[df1.title == title_user].index.values[0]
+    #Create a list of tuples in the form (index,similarity)
+    scores = list(enumerate(cs[indices]))
+    # Sort the list of similar books in descending order
+    sorted_score = sorted(scores, key=lambda x:x[1], reverse=True)
+    sorted_score = sorted_score[1:]
+    # Create a loop to print the first 5 books from the sorted list
+    j=0
+    print('The 5 most recommended books to '+title_user+' are:\n')
+    for item in sorted_score:
+        book_title = df1[df1.index == item[0]]['title'].values[0]
+        print(j+1, book_title)
+        j=j+1
+        if j >=5:
+            break
+
+#Complete suggestion function -combines the 3 functions above
+def give_recommendationComplete(title_user):
     try:
-        #1.Step - convert the description from combined_features to a matrix of word counts
-        cm = CountVectorizer().fit_transform(df1['combined_features'])
-        #2.Get the cosine_simmilarity from the count matrix
-        cs = cosine_similarity(cm)
-        #Get index of title of user input
-        indices = df1[df1.title == title_user].index.values[0]
-        #Create a list of tuples in the form (index,similarity)
-        scores = list(enumerate(cs[indices]))
-        # Sort the list of similar books in descending order
-        sorted_score = sorted(scores, key=lambda x:x[1], reverse=True)
-        sorted_score = sorted_score[1:]
-        # Create a loop to print the first 5 books from the sorted list
-        j=0
-        print('The 5 most recommended books to '+title_user+' are:\n')
-        for item in sorted_score:
-            book_title = df1[df1.index == item[0]]['title'].values[0]
-            print(j+1, book_title)
-            j=j+1
-            if j >=5:
-                break
-            #return 'The 5 most recommended books to '+title_user+' are:\n'
+        give_5bookrecommendationsCount(title_user)
     except:
-        suggestions_user_input(title_user)
+        try:
+            assume_book_title(title_user)
+        except:
+            suggestions_other_title(title_user)       
+
 
 
 
